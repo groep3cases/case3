61e0b6b49becb4d7f17799e69c3e6b7c09ac2d3b
sellig3002 <112904390+sellig3002@users.noreply.github.com>
2025-10-08 16:25:15 +0200

    Update kaggleapi.py

--- kaggleapi.py
+++ kaggleapi.py
@@ -2,7 +2,9 @@ from kaggle.api.kaggle_api_extended import KaggleApi
 from pathlib import Path
 import os
 import streamlit as st
+import requests, zipfile, io
 
+@st.cache_data(show_spinner=False)
 def download_dataset(dataset):
     os.environ["KAGGLE_USERNAME"] = st.secrets["kaggle"]["username"]
     os.environ["KAGGLE_KEY"] = st.secrets["kaggle"]["key"]
@@ -14,3 +16,8 @@ def download_dataset(dataset):
     data_dir.mkdir(exist_ok=True)
 
     api.dataset_download_files(dataset, path=data_dir, unzip=True)
+
+@st.cache_data(show_spinner=False)
+def download_kaarten(url):
+    response = requests.get(url)
+    zipfile.ZipFile(io.BytesIO(response.content)).extractall("kaarten")
